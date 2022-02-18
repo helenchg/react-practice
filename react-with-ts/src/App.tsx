dc59a4eef65c17afa3e4e45344c4bcba2b7162af
Elenack <helenchg93@hotmail.com>
2022-02-18 18:21:29 -0500

    commit to master

---
+++ react-with-ts/src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useRef } from "react";
+import "./App.css";
+import { Toggle } from "./Toggle";
+
+function App() {
+  const buttonRef = useRef(null);
+
+  return (
+    <div className="App">
+      <h1>Hooks</h1>
+      <button
+        onClick={() => {
+          buttonRef.current.alterToggle();
+        }}
+      >
+        PARENT BUTTON
+      </button>
+      <Toggle ref={buttonRef} />
+    </div>
+  );
+}
+
+export default App;
